03b6e85aa56b440a2138668d95244701cc9be5ad
Michael Kreil <github@michael-kreil.de>
2024-03-03 00:12:38 +0100

    add dts

--- rollup.config.js
+++ rollup.config.js
@@ -2,30 +2,38 @@ import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
+import { dts } from 'rollup-plugin-dts';
 
-export default {
-	input: 'src/index.ts',
-	output: [
-		{
-			file: 'dist/maplibre-gl-versatiles-styler.js',
-			format: 'umd',
-			name: 'VersatilesStyler',
-			sourcemap: true,
-		},
-		{
-			file: 'dist/maplibre-gl-versatiles-styler.min.js',
-			format: 'umd',
-			name: 'VersatilesStyler',
-			sourcemap: true,
-			plugins: [
-				terser()
-			]
-		},
-	],
-	treeshake: true,
-	plugins: [
-		commonjs(),
-		nodeResolve(),
-		typescript()
-	]
-};
+export default [
+	{
+		input: 'src/index.ts',
+		output: [
+			{
+				file: './dist/maplibre-gl-versatiles-styler.js',
+				format: 'umd',
+				name: 'VersatilesStyler',
+				sourcemap: true,
+			},
+			{
+				file: './dist/maplibre-gl-versatiles-styler.min.js',
+				format: 'umd',
+				name: 'VersatilesStyler',
+				sourcemap: true,
+				plugins: [
+					terser()
+				]
+			},
+		],
+		treeshake: true,
+		plugins: [
+			commonjs(),
+			nodeResolve(),
+			typescript()
+		]
+	},
+	{
+		input: './dist/dts/index.d.ts',
+		output: [{ file: 'dist/maplibre-gl-versatiles-styler.d.ts', format: 'es' }],
+		plugins: [dts()],
+	},
+]
